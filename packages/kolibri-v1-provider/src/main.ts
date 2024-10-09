f1e234d394705ca7907057110457e4b12341cb7c
Stefan Dietz <mail@stefan-dietz.eu>
2024-10-09 11:20:40 +0200

    Add sample with two identical KoliBri versions and Toaster usage

--- packages/kolibri-v1-provider/src/main.ts
+++ packages/kolibri-v1-provider/src/main.ts
@@ -2,6 +2,8 @@ import { register } from '@public-ui/components';
 import { defineCustomElements } from '@public-ui/components/dist/loader';
 import { DEFAULT } from '@public-ui/theme-default';
 
+export * as KoliBri from '@public-ui/components';
+
 export async function registerKolibri() {
 	await register(DEFAULT, defineCustomElements);
 }
