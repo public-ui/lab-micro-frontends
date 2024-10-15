63f92e3e7ea803d83d5777a44bca30eb650e4c81
Stefan Dietz <mail@stefan-dietz.eu>
2024-10-15 12:28:25 +0200

    Configure both providers/modules with the same tag name transformer

--- packages/kolibri-v1-provider/src/main.ts
+++ packages/kolibri-v1-provider/src/main.ts
@@ -1,9 +1,19 @@
-import { register } from '@public-ui/components';
+import { bootstrap } from '@public-ui/components';
 import { defineCustomElements } from '@public-ui/components/dist/loader';
 import { DEFAULT } from '@public-ui/theme-default';
 
 export * as KoliBri from '@public-ui/components';
 
+const transformTagName = (tagName: string) => {
+	return `${tagName}-v2`;
+};
+
 export async function registerKolibri() {
-	await register(DEFAULT, defineCustomElements);
+	await bootstrap(DEFAULT, [], {
+		transformTagName,
+	}).then(() => {
+		void defineCustomElements(window, {
+			transformTagName,
+		} as any); // https://github.com/ionic-team/stencil/issues/2847
+	});
 }
